139876cd2b9954e77fbc4afde64d133c11a39935
bundamm <tomcio.szewa@gmail.com>
2023-02-24 09:44:38 +0100

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,52 @@
+#zad1
+a = "********************"
+for i in range(4):
+    print(a)
+print('\n')
+#zad2
+n = 4
+z= n - 2
+
+#for i in range(n):
+#    if(i == 0 or i == n-1):
+#        print("* "*20)
+#    else:
+#        print("*", "  "* 18, "*", )
+
+print("*************************")
+print("*                       *")
+print("*                       *")
+print("*************************")
+
+
+#zad3
+n = 10
+for i in range(n+1):
+    print("*"*i)
+
+#zad4
+print((512-282)/(47*48+5))
+
+#zad5
+x = 7
+n = 5
+
+
+#for i in range(1,n+1):
+ #   if(i == n):
+  #      print(x*i)
+  #  else:
+  #      print(x*i, '---', sep='', end='')
+
+#zad6
+p = 'Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker'
+print(p)
+
+#zad7
+i = input("Podaj pierwsza liczbe:")
+ii = input("Podaj druga liczbe:")
+print(int(i)*int(ii))
+
+#zad8
+o = input("Wpisz wage w kilogramach:")
+print("Waga w funtach: ", int(o)*2.20462)
